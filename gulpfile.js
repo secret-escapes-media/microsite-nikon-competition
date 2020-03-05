6fb0798da17f6f5e5380219e17c9e3420bbfe0ca
hamishgray <hamish.gray@secretescapes.com>
2020-03-05 11:51:59 +0000

    structure with interviews and competition integrated into independent pages

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ function browserSyncServe() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/template': '_site/'
+        '/uk/2020/nikon-golden-hour': '_site/'
       }
     }
   });
